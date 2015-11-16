746012ec6094de33e7077a58651e08137f43688d
koby becker <beckerkoby@gmail.com>
2015-11-15 17:15:44 -0800

    add timing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include "integrate.h"
 #include "inheritanceExpressions.h"
 #include "templatedExpressions.h"
+#include <chrono>
 
 int main()
 {
     std::shared_ptr<Variable> x = std::make_shared<Variable>();
+    
     auto e = x*x*x + 2*x*x + x*3 + 55;
-    std::cout << (*e)(5.0) << integrate(*e, 0, 10);
+ 
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+    for (int i = 0; i < 20; ++i)
+    {
+        integrate(*e, 0, 10);
+    }
+
+    std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
+
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() <<std::endl;
+    
     return 0;
 }
